b31fb03fe5063542440dd495790c1d6e15493b0b
Ce Zhang <czhang@cs.wisc.edu>
2012-12-05 20:05:36 -0600

    Coref and Dependency Path

--- src/nlp/runNER.java
+++ src/nlp/runNER.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,6 +20,8 @@ import java.util.zip.GZIPOutputStream;
 
 import org.apache.tika.exception.TikaException;
 
+import edu.stanford.nlp.dcoref.CorefChain;
+import edu.stanford.nlp.dcoref.CorefChain.CorefMention;
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
@@ -26,14 +29,59 @@ import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CorefCoreAnnotations.CorefChainAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
+import edu.stanford.nlp.trees.semgraph.SemanticGraph;
+import edu.stanford.nlp.trees.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.trees.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.util.CoreMap;
+
+class myWord{
+
+	public String word;
+	public String pos;
+	public String lemma;
+	public String ne;
+	
+	public int corefID;
+	
+	public int dep_partent = 0;
+	public String dep_class = "_";
+	
+	public myWord(String _word, String _pos, String _lemma, String _ne, int _corefID){
+		word = _word;
+		pos = _pos;
+		lemma = _lemma;
+		ne = _ne;
+		
+		corefID = _corefID;
+	}
+	
+}
+
+class mySentence{
+	ArrayList<myWord> words = new ArrayList<myWord>();
+	
+	public void pushWord(myWord word){
+		words.add(word);
+	}
+}
+
+class myDocument{
+	ArrayList<mySentence> sentences = new ArrayList<mySentence>();
+	
+	public void pushSentence(mySentence sent){
+		sentences.add(sent);
+	}
+}
 
 public class runNER extends SimpleFunction {
 	private static boolean silent = true;
 	private static boolean input_compressed = false;
-	private static boolean compress_output = true;
+	private static boolean compress_output = false;
 	
 	//public static String doNER(String )
 
@@ -60,8 +108,13 @@ public class runNER extends SimpleFunction {
 	    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 	    ArrayList<String> parsedSentences = new ArrayList<String>();
 	    
+	    myDocument mydoc = new myDocument();
+	    
 	    int sentid = 0;
 	    for(CoreMap sentence: sentences) {
+	    	
+	    	mySentence mysent = new mySentence();
+	    	
 	    	sentid = sentid + 1;
 		    StringBuffer sb = new StringBuffer();
 	    	sb.append("<SENT docid=\"" + docid + "\" sentid=\"" + sentid + "\">\n");
@@ -74,7 +127,13 @@ public class runNER extends SimpleFunction {
 	    		String lemma = token.get(LemmaAnnotation.class);  
 	    		String ne = token.get(NamedEntityTagAnnotation.class);   
 	    		sb.append(wordid + "\t" + word + "\t" + pos + "\t" + ne + "\t" + lemma + "\n");
+	    		
+	    		mysent.pushWord(new myWord(word, pos, lemma, ne, -1));
+	    		
 	    	}
+	    	
+	    	mydoc.pushSentence(mysent);
+	    	
 	    	sb.append("</SENT>");
 	    	parsedSentences.add(sb.toString());
 	    }
@@ -85,7 +144,7 @@ public class runNER extends SimpleFunction {
 	static public void main(String[] args) throws IOException, TikaException{
 		if (!silent) System.err.println("Starting NER...");
 	    Properties props = new Properties();
-	    props.put("annotators", "tokenize, cleanxml, ssplit, pos, lemma, ner");
+	    props.put("annotators", "tokenize, cleanxml, ssplit, pos, lemma, ner, parse, dcoref");
 	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 	    
 	    String filename = args[0];
@@ -134,10 +193,15 @@ public class runNER extends SimpleFunction {
 			    pipeline.annotate(document);
 			    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 			    
+			    myDocument mydoc = new myDocument();
+			    
 			    int sentid = 0;
 			    for(CoreMap sentence: sentences) {
+			    	
+			    	mySentence mysent = new mySentence();
+			    	
 			    	sentid = sentid + 1;
-			    	os.write("<SENT id=\"" + docid + "_SENT_" + sentid + "\">\n");
+			    	//os.write("<SENT id=\"" + docid + "_SENT_" + sentid + "\">\n");
 			    	
 			    	int wordid = 0;
 			    	for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
@@ -146,9 +210,62 @@ public class runNER extends SimpleFunction {
 			    		String pos = token.get(PartOfSpeechAnnotation.class);
 			    		String lemma = token.get(LemmaAnnotation.class);  
 			    		String ne = token.get(NamedEntityTagAnnotation.class);   
-			    		os.write(wordid + "\t" + word + "\t" + pos + "\t" + ne + "\t" + lemma + "\n");
+			    		//os.write(wordid + "\t" + word + "\t" + pos + "\t" + ne + "\t" + lemma + "\n");
+			    		mysent.pushWord(new myWord(word, pos, lemma, ne, -1));
 			    	}
-			    	os.write("</SENT>\n\n");
+			    	//os.write("</SENT>\n\n");
+			    	
+			    	Tree tree = sentence.get(TreeAnnotation.class);
+			    	
+			    	SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
+			    	
+			    	for(SemanticGraphEdge edge : dependencies.getEdgeSet()){
+			    		
+			    		myWord source = mysent.words.get(edge.getSource().index()-1);
+			    		myWord target = mysent.words.get(edge.getTarget().index()-1);
+			    		
+			    		target.dep_class = edge.toString();
+			    		target.dep_partent = edge.getSource().index();
+			    		
+			    	}
+			    	
+			    	
+			    	mydoc.pushSentence(mysent);
+			    	
+			    	//System.out.println(depden)
+			    }
+			    
+			    Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class);
+			    
+			    for(Integer clusterID : graph.keySet()){
+			    	CorefChain chain = graph.get(clusterID);
+			    	
+			    	for(CorefMention cm : chain.getCorefMentions()){
+			    		//System.out.println("Coref" + clusterID + ":  SENT-" + cm.sentNum + " " + "WORD-" + cm.startIndex + " ~ WORD-" + cm.endIndex + " " + cm.mentionSpan); 
+			    		for(int woffset =  cm.startIndex; woffset < cm.endIndex; woffset ++){
+			    			mydoc.sentences.get(cm.sentNum-1).words.get(woffset-1).corefID = clusterID;
+			    		}
+			    	
+			    	}
+			    }
+			    
+			    
+			    sentid = 0;
+			    for(mySentence mysent : mydoc.sentences){
+			    	
+			    	sentid = sentid + 1;
+			    	os.write("<SENT id=\"" + docid + "_SENT_" + sentid + "\">\n");
+			    	
+			    	int wordid = 0;
+			    	for(myWord myword : mysent.words){
+			    		wordid = wordid + 1;
+			    		os.write(wordid + "\t" + myword.word + "\t" + myword.pos + 
+			    				                  "\t" + myword.ne + "\t" + myword.lemma + 
+			    				                  "\t" + myword.dep_class + "\t" + myword.dep_partent +
+			    				                  "\t" + myword.corefID + "\n");
+			    	}
+			    	
+			    	os.write("</SENT>\n");
 			    }
 			    
 			    content = "";
